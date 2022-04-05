76f34ac61309e024d9d0387dcba8e9947d68eb0a
Mazy Toufik <mazytoufik@outlook.com>
2022-04-05 23:11:21 +0200

    Commit init storage file services

---
+++ storage-service-alimapp/src/main/java/io/mazy/storageservicealimapp/StorageServiceAlimappApplication.java
@@ -0,0 +1,13 @@
+package io.mazy.storageservicealimapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageServiceAlimappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageServiceAlimappApplication.class, args);
+    }
+
+}
---
+++ storage-service-alimapp/src/test/java/io/mazy/storageservicealimapp/StorageServiceAlimappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mazy.storageservicealimapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceAlimappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
