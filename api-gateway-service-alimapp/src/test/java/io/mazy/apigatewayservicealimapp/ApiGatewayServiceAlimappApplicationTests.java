31be4ed1fff31ad133bf131dbdff6cd3ad90140e
Mazy Toufik <mazytoufik@outlook.com>
2022-04-05 23:25:21 +0200

    Commit init api gateway file services + disable discocery all propreties files

---
+++ api-gateway-service-alimapp/src/test/java/io/mazy/apigatewayservicealimapp/ApiGatewayServiceAlimappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mazy.apigatewayservicealimapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceAlimappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
