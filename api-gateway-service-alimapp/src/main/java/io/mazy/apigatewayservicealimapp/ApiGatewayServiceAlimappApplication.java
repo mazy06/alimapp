52114536f7edf7d71e6e047a34407afb753a3b0d
Mazy Toufik <mazytoufik@outlook.com>
2022-04-06 14:39:03 +0200

    Commit fix compatability version maven dependencies + add banner to chat and gateway services + add dynamic and static route locator for local service and external api service

--- api-gateway-service-alimapp/src/main/java/io/mazy/apigatewayservicealimapp/ApiGatewayServiceAlimappApplication.java
+++ api-gateway-service-alimapp/src/main/java/io/mazy/apigatewayservicealimapp/ApiGatewayServiceAlimappApplication.java
@@ -2,12 +2,41 @@ package io.mazy.apigatewayservicealimapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayServiceAlimappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayServiceAlimappApplication.class, args);
+    }
+
+    /**
+     * Dynamic route to local services
+     * @param rdc
+     * @param dlp
+     * @return
+     */
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
+
+    /**
+     * Static route to external api services
+     * @param builder
+     * @return
+     */
+    @Bean
+    RouteLocator staticRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().build();
     }
 
 }
--- storage-service-alimapp/src/main/java/io/mazy/storageservicealimapp/StorageServiceAlimappApplication.java
+++ storage-service-alimapp/src/main/java/io/mazy/storageservicealimapp/StorageServiceAlimappApplication.java
@@ -2,8 +2,10 @@ package io.mazy.storageservicealimapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StorageServiceAlimappApplication {
 
     public static void main(String[] args) {
