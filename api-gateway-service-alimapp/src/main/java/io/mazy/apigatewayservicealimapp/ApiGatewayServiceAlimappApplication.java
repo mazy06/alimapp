ec78f1fd36a80e467038dee84f2e29ed5b0f15fd
Mazy Toufik <mazytoufik@outlook.com>
2022-04-07 01:05:11 +0200

    Commit add custom banner to contact, storage and discovery services + add 2 static api test and config one circuit breaker

--- api-gateway-service-alimapp/src/main/java/io/mazy/apigatewayservicealimapp/ApiGatewayServiceAlimappApplication.java
+++ api-gateway-service-alimapp/src/main/java/io/mazy/apigatewayservicealimapp/ApiGatewayServiceAlimappApplication.java
@@ -9,6 +9,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -36,7 +39,45 @@ public class ApiGatewayServiceAlimappApplication {
      */
     @Bean
     RouteLocator staticRoutes(RouteLocatorBuilder builder) {
-        return builder.routes().build();
+        return builder.routes()
+                .route(r -> r
+                        .path("/**")
+                        .filters(f -> f
+                                .addRequestHeader("x-rapidapi-host", "wft-geo-db.p.rapidapi.com")
+                                .addRequestHeader("x-rapidapi-key", "351906158emshae91b1f03f78323p1cf294jsn33b01df9c3ef")
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(h -> h.setName("countries").setFallbackUri("forward:/break/countrystatecity_api"))
+                        )
+                        .uri("https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions"))
+                .route(r -> r                                           //endpoint salat hour not working -> must be to fix !!!!
+                        .path("/muslimsalat/**")
+                        .filters(f -> f
+                                .addRequestHeader("x-rapidapi-host", "muslimsalat.p.rapidapi.com")
+                                .addRequestHeader("x-rapidapi-key", "351906158emshae91b1f03f78323p1cf294jsn33b01df9c3ef")
+                                .rewritePath("/muslimsalat/(?<segment>.*)", "/${segment}"))
+                        .uri("https://muslimsalat.p.rapidapi.com"))
+                .build();
     }
 
+}
+@RestController
+@RequestMapping("/break")
+class CircuitBreakerRestController {
+    @GetMapping("/message")
+    public String message(){
+        return "Hystrix called fallback services ";
+    }
+    @GetMapping("/countrystatecity_api")
+    public RouteLocator country_state_city_api(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r
+                        .path("/**")
+                        .filters(f -> f
+                                .addRequestHeader("X-CSCAPI-KEY", "https://countrystatecity.in/")
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                .circuitBreaker(h -> h.setName("countries").setFallbackUri("forward:/break/message"))
+                        )
+                        .uri("https://countrystatecity.in/"))
+                .build();
+    }
 }
